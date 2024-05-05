2decd78c0d322e9b1bcfa97d0fa89ce1be5af68e
Justin Chase <justin.m.chase@gmail.com>
2024-05-05 12:20:39 -0500

    use import attributes to import json files

--- src/runtime/resolvers/json.resolver.ts
+++ src/runtime/resolvers/json.resolver.ts
@@ -4,19 +4,12 @@ import { IModuleResolver } from "./resolver.ts";
 export class JsonResolver implements IModuleResolver {
   public readonly extension = ".json";
   async resolveModule(moduleUrl: URL): Promise<IModuleDeclaration> {
-    const json = await this.fetchContent(moduleUrl);
-    return await JSON.parse(json);
-  }
-
-  async fetchContent(moduleUrl: URL) {
-    const url = new URL(moduleUrl);
-    if (url.protocol === "file:") {
-      return await Deno.readTextFile(url.pathname);
-    } else if (url.protocol === "http:" || url.protocol === "https:") {
-      const result = await fetch(moduleUrl);
-      return await result.text();
-    } else {
-      throw new Error(`Uknonwn protocol ${url.protocol}`);
+    const module = await import(moduleUrl.href, { with: { type: "json" } })
+    if (!module.default) {
+      throw new Error(
+        `Imported module ${moduleUrl} must export an IModuleDeclaration as a default export`,
+      )
     }
+    return module.default;
   }
 }
